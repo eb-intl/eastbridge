48fee13453f78001704051bff1fe97a5eedb68c8
rangertaha <tahasaidi@gmail.com>
2016-08-25 04:05:30 -0400

    updates

--- apps/metatags/templatetags/metatags.py
+++ apps/metatags/templatetags/metatags.py
@@ -11,17 +11,19 @@ def keywords(objs=None):
     words = []
     if isinstance(objs, QuerySet):
         for obj in objs:
-            for tag in obj.tags.all():
+            if hasattr(obj, 'tag'):
+                for tag in obj.tags.all():
+                    if len(tag.long_name) > 0:
+                        words.append(tag.long_name)
+                    if len(tag.short_name) > 0:
+                        words.append(tag.short_name)
+    elif objs:
+        if hasattr(objs, 'tag'):
+            for tag in objs.tags.all():
                 if len(tag.long_name) > 0:
                     words.append(tag.long_name)
                 if len(tag.short_name) > 0:
                     words.append(tag.short_name)
-    elif objs:
-        for tag in objs.tags.all():
-            if len(tag.long_name) > 0:
-                words.append(tag.long_name)
-            if len(tag.short_name) > 0:
-                words.append(tag.short_name)
     else:
         tags = Tag.objects.all()
         for tag in tags:
