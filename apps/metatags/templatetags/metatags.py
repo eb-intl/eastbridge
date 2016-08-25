b3bc25e1f5dea7872122c4a84817d61485f62576
rangertaha <tahasaidi@gmail.com>
2016-08-25 03:28:01 -0400

    updates

--- apps/metatags/templatetags/metatags.py
+++ apps/metatags/templatetags/metatags.py
@@ -1,11 +1,13 @@
 from django import template
 from django.db.models.query import QuerySet
 
+from .. models import Tag
+
 register = template.Library()
 
 
 @register.filter(name='keywords')
-def keywords(objs):
+def keywords(objs=None):
     words = []
     if isinstance(objs, QuerySet):
         for obj in objs:
@@ -14,8 +16,15 @@ def keywords(objs):
                     words.append(tag.long_name)
                 if len(tag.short_name) > 0:
                     words.append(tag.short_name)
-    else:
+    elif objs:
         for tag in objs.tags.all():
+            if len(tag.long_name) > 0:
+                words.append(tag.long_name)
+            if len(tag.short_name) > 0:
+                words.append(tag.short_name)
+    else:
+        tags = Tag.objects.all()
+        for tag in tags:
             if len(tag.long_name) > 0:
                 words.append(tag.long_name)
             if len(tag.short_name) > 0:
