f59a945c11945b600376b124e176d136415a9184
rangertaha <tahasaidi@gmail.com>
2016-08-25 04:27:37 -0400

    updates

--- apps/metatags/templatetags/metatags.py
+++ apps/metatags/templatetags/metatags.py
@@ -11,14 +11,14 @@ def keywords(objs=None):
     words = []
     if isinstance(objs, QuerySet):
         for obj in objs:
-            if hasattr(obj, 'tag'):
+            if hasattr(obj, 'tags'):
                 for tag in obj.tags.all():
                     if len(tag.long_name) > 0:
                         words.append(tag.long_name)
                     if len(tag.short_name) > 0:
                         words.append(tag.short_name)
     elif objs:
-        if hasattr(objs, 'tag'):
+        if hasattr(objs, 'tags'):
             for tag in objs.tags.all():
                 if len(tag.long_name) > 0:
                     words.append(tag.long_name)
