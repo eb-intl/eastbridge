203ca3cc3c1db489cd5bb32e686e441c213c10ba
rangertaha <tahasaidi@gmail.com>
2016-08-24 21:51:37 -0400

    updates

--- apps/metatags/templatetags/metatags.py
+++ apps/metatags/templatetags/metatags.py
@@ -10,12 +10,16 @@ def keywords(objs):
     if isinstance(objs, QuerySet):
         for obj in objs:
             for tag in obj.tags.all():
-                words.append(tag.short_name)
-                words.append(tag.long_name)
+                if len(tag.long_name) > 0:
+                    words.append(tag.long_name)
+                if len(tag.short_name) > 0:
+                    words.append(tag.short_name)
     else:
         for tag in objs.tags.all():
-            words.append(tag.short_name)
-            words.append(tag.long_name)
+            if len(tag.long_name) > 0:
+                words.append(tag.long_name)
+            if len(tag.short_name) > 0:
+                words.append(tag.short_name)
 
     cwords = list(set(words))
     return ','.join(cwords)
